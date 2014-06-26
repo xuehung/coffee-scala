87892d77a7eabb3f544ac8b5be126b8e53ea1faf
xuehung <xuehung@gmail.com>
2014-06-25 22:55:14 -0400

    added index

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,12 +3,11 @@ package controllers
 import play.api.mvc._
 
 
-object PaidApplication extends Controller with Authentication with PremiumUsersOnly with BalanceCheck {
+object Application extends Controller {
 
-  override def getRequiredBalance = 8
-
-  def helloUser = AuthenticateMe {
-    user => Ok(s"hello ${user.email}")
+  def index = Action {
+    //Ok("hello world")
+    Ok(views.html.index("hello world"))
   }
 
 }
